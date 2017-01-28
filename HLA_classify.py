6f597c420c29a8467355f4d5ee743099e89a07e4
winstonhe9 <winstonhe9@gmail.com>
2017-01-27 22:15:35 -0500

    Update HLA_classify.py

--- HLA_classify.py
+++ HLA_classify.py
@@ -1,14 +1,4 @@
-"""
-import numpy
-from keras.datasets import imdb
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers.embeddings import Embedding
-from keras.preprocessing import sequence
-# fix random seed for reproducibility
-numpy.random.seed(7)
-"""
+
 import numpy
 from keras.models import Sequential
 from keras.layers import Dense
